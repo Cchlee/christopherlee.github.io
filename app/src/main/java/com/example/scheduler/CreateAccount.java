b1fe38683e4764b65691e5ba8cdbc96c2d5a7a02
Yunhee Hyun <yuhy@sas.upenn.edu>
2019-03-16 23:32:01 -0400

    Notes for group members:
    Tasya: created UserSchedule activity
    Chris: SecondPage is being used as the Home page. Created ViewCommunity activity
    Daphne: created Scheduler activity
    
    I can't (don't think I can) finish AddProject until ViewCommunity is done. Can't test CreateCommunity/AddProject until ViewCommunity/Home page finished.

---
+++ app/src/main/java/com/example/scheduler/CreateAccount.java
@@ -0,0 +1,64 @@
+package com.example.scheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.content.Intent;
+import data.*;
+
+
+public class CreateAccount extends AppCompatActivity {
+    private EditText name;
+    private EditText password;
+    private RadioGroup accountTypes;
+    private boolean isUser = false;
+    private Button createButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        // Name and Password
+        name = findViewById(R.id.newAccName);
+        password = findViewById(R.id.newAccPassword);
+
+        // Which account (user or leader)
+        accountTypes = findViewById(R.id.accountType);
+        accountTypes.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.User:
+                        isUser = true;
+                        break;
+                    default:
+                        isUser = false;
+                        break;
+                }
+            }
+        });
+
+        // Create account and how to proceed from there
+        createButton = findViewById(R.id.createButton);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUser();
+            }
+        });
+    }
+
+    private void checkUser() {
+        if (isUser) {
+            User user = new User(name.toString(), password.toString());
+            Intent intent = new Intent(CreateAccount.this, UserSchedule.class);
+            intent.putExtra("User", user);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(CreateAccount.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/scheduler/CreateCommunity.java
@@ -0,0 +1,30 @@
+package com.example.scheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+import android.content.Intent;
+import data.*;
+
+public class CreateCommunity extends AppCompatActivity {
+    private EditText name;
+    private EditText description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_community);
+
+        // Name and Description
+        name = findViewById(R.id.newCommName);
+        description = findViewById(R.id.newCommDescription);
+
+        // Create new community
+        Community community = new Community(name.toString(), description.toString());
+
+        // Go back to homepage
+        Intent intent = new Intent(CreateCommunity.this, SecondPage.class);
+        intent.putExtra("Community", community);
+        startActivity(intent);
+    }
+}
