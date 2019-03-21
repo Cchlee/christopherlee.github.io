bf817c856cd8a4370b942f70911040e407e4de3f
daphnekontogiorgos-heintz <daphnekheintz@gmail.com>
2019-03-21 18:48:00 -0400

    timeslot from TimeSlot -> int schedule from map to boolean array TreeSet -> set

--- app/src/main/java/data/Project.java
+++ app/src/main/java/data/Project.java
@@ -8,7 +8,7 @@ public class Project implements Serializable {
     private String description;
     private int timeslot;
     private Map<String, Integer> roles; // role types and number of people per role required
-    private TreeSet<User> users;
+    private Set<User> users;
     private Map<String, List<User>> userAssignment;
 
     public Project(String name, String description, String communityCreatedIn,
@@ -17,7 +17,7 @@ public class Project implements Serializable {
         this.description = description;
         this.roles = roles;
         this.communityCreatedIn = communityCreatedIn;
-        users = new TreeSet<User>();
+        users = new HashSet<User>();
         userAssignment = new HashMap<String, List<User>> ();
     }
 
@@ -53,11 +53,11 @@ public class Project implements Serializable {
         this.roles = roles;
     }
 
-    public TreeSet<User> getUsers() {
+    public Set<User> getUsers() {
         return users;
     }
 
-    public void setUsers(TreeSet<User> users) {
+    public void setUsers(Set<User> users) {
         this.users = users;
     }
 
